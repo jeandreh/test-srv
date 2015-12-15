fa74872b56267aa6b21db962f8c1a0af65f6e0bb
Jeandre Sutil <jeandreh@gmail.com>
2015-12-15 11:30:21 +1100

    Setting routes for WS api

--- config/routes.rb
+++ config/routes.rb
@@ -61,9 +61,10 @@ Rails.application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+    # Directs /admin/products/* to Admin::ProductsController
+    # (app/controllers/admin/products_controller.rb)
+    # resources :products
+  end
 end
